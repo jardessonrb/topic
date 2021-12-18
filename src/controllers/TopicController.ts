15cacc2b38771528764d62470a309f15bcfb42ea
Jardesson Ribeiro <jardessonrs1117@gmail.com>
2021-12-18 10:05:23 -0300

    created method createTopic

--- src/controllers/TopicController.ts
+++ src/controllers/TopicController.ts
@@ -3,17 +3,31 @@ import { getConnection } from "typeorm";
 import { User } from "../models/User";
 import { TopicRepository } from "../repositories/TopicRepository";
 import { UserRepository } from "../repositories/UserRepository";
-import { ResponseError } from "../types/ResponseError";
-import { ResponseErrorServer } from "../types/ResponseErrorServer";
-import { ResponseSuccess } from "../types/ResponseSuccess";
+import { ResponseError, ResponseErrorServer, ResponseSuccess } from "../types";
+import * as Yup from "yup";
 
 class TopicController{
 
   static async createTopic(request: Request, response: Response ): Promise<Response>{
     const { title, body, userId } = request.body;
     let user: User;
-    const topicRepository = getConnection().getCustomRepository(TopicRepository);
 
+    const schemaValidation = Yup.object().shape({
+      title: Yup.string().required("O titulo do topico é obrigatorio"),
+      body: Yup.string().required("O conteudo do topico é obrigatorio"),
+      userId: Yup.string().required("O usuario é obrigatorio").uuid("Identificador não válido")
+    });
+
+    try {
+      await schemaValidation.validate({title, body, userId}, {
+        abortEarly: false
+      });
+    } catch (error) {
+      const res: ResponseError = {message: "Erro de validação", type: "error validation", errors: error.errors}
+        return response.status(403).json(res);
+    }
+
+    const topicRepository = getConnection().getCustomRepository(TopicRepository);
     try {
       user = await getConnection().getCustomRepository(UserRepository).findById(userId);
       if(!user){
@@ -39,10 +53,9 @@ class TopicController{
       const res: ResponseSuccess = {message: "Topico criado com sucesso", type: "success", body: topic};
       return response.status(200).json(res);
     } catch (error) {
-
+      const res: ResponseErrorServer = {message:"Erro no servidor", type: "error server"}
+      return response.status(500).json(res);
     }
-
-    return response;
   }
 }
 
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+import { ResponseError } from "./ResponseError";
+import { ResponseSuccess } from "./ResponseSuccess";
+import { ResponseErrorServer } from "./ResponseErrorServer";;
+
+
+export { ResponseError, ResponseSuccess, ResponseErrorServer };
