56dfd1506702ef06f5cc0fac3bb4fcdde70fa128
Jardesson Ribeiro <jardessonrs1117@gmail.com>
2021-12-19 19:33:30 -0300

    Created a method to list the comments of each topic, bringing the name of the user who commented

--- src/repositories/CommentRepository.ts
+++ src/repositories/CommentRepository.ts
@@ -4,9 +4,24 @@ import { Topic } from "../models/Topic";
 
 @EntityRepository(Comment)
 class CommentRepository extends Repository<Comment>{
+
   async findCommentByTopic(topic: Topic): Promise<Comment[]>{
     try {
-      return await this.find({where: {topic}});
+      const result = await this.createQueryBuilder("comments")
+                                  .select(["comments.id_comment", "comments.body_comment","comments.created_at", "user.name_user"])
+                                  .innerJoinAndSelect("comments.user", "user")
+                                  .where("comments.topicId = :id", {id: topic.id})
+                                  .execute();
+      const comments = result.map((comment: any) => {
+        return {
+          body: comment.body_comment,
+          createdAt: comment.created_at,
+          id: comment.created_at,
+          name: comment.name_user
+        };
+      })
+
+      return comments;
 
     } catch (error) {
       throw new Error();
